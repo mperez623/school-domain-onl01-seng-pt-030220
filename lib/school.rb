59781657081ef292e0c8509fb541567a0fd66665
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-26 00:36:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,8 @@ class School
   def add_student(student, grade)
     if @roster[grade]
       @roster[grade] << student 
-    else @roster[grade] = student
+    else 
+      @roster[grade] = student
   end
 end
   
